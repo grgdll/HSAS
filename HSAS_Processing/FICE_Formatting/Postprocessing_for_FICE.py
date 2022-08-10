378c679655fb4e89a13c65db56984343dbe94a49
Tom Jordan <tjor@pmpc1460.npm.ac.uk>
2022-08-10 12:44:33 +0100

    Code now writes test output in FICE format. Commit includes: (i) L2 unpack function correction, (ii) rrs and nLw now included in down-sampling and write functions

--- HSAS_Processing/FICE_Formatting/Postprocessing_for_FICE.py
+++ HSAS_Processing/FICE_Formatting/Postprocessing_for_FICE.py
@@ -54,7 +54,7 @@ def unpack_srf(dir_srf):
     '''Function to unpack OLCI SRF in np array format'''
    
     srf_data = nc.Dataset(dir_srf + '/S3A_OL_SRF_20160713_mean_rsr.nc4') # load OLCI SRF
-    srf_bands =  srf_data['nominal_centre_wavelength'][0:16]  # band centres where Hsas has data
+    srf_bands =  srf_data['nominal_centre_wavelength'][0:19]  # band centres up to 900 nm
     # band_width = srf_data['bandwidth_fwhm'][0:16] - not needed
     srf_wv = np.array(srf_data['mean_spectral_response_function_wavelength'])
     srf = np.array(srf_data['mean_spectral_response_function'])
@@ -80,31 +80,31 @@ def unpack_L1(fn_L1):
     return time, windspeed, Es, Lt, Li, wv
 
 
-def unpack_L2(fn_L2, time_bins , wv_bins):
+def unpack_L2(fn_L2, time_L1, wv):
     ''' unpacks relevant L2 data in np array format. Adjustment is made so that 
-    size of output matches L1 data. Timestamps that not pass quality control (
-    (Lt glint filter) are filled with NaNs'''
+    size of output matches L1 data. Timestamps that do not pass quality control are NaN padded'''
   
     L2 = io.loadmat(fn_L2[0], squeeze_me = True, struct_as_record = False) 
     
-    # load files in L2 format
+    # load files in L2 format- L2 subscript indicates matrices are undersized
     Rrs_L2 = L2['L2'].Rrs.data 
     rho_L2 = L2['L2'].rho 
     exLwn_L2 = L2['L2'].exLwn.data
-    qc_ind = L2['L2'].glint_filter.indices_passed_qc  # indices that were kept from L1 data
+    time_L2 = np.array(numeric_to_UTC_time(L2['L2'].gps.time)) 
+    
+    # indicies in 1 time that match L2 time (i.e. pass overall QC)
+    L1_matches = np.intersect1d(time_L1, time_L2, return_indices = True)[1]
 
-    # convert to length of L1 data, with nan-padding added where no data
-    qc_mask = np.zeros(time_bins) # 1 == data, 0 === no data
-    Rrs = np.nan*np.ones([time_bins, wv_bins])
-    exLwn = np.nan*np.ones([time_bins, wv_bins])
-    rho = np.nan*np.ones(time_bins)
-    print(len(qc_ind)) # note - qc ind does not currently match length of Rrs - issue here
-    for i in range(len(qc_ind)): # fill L1-size stuctures
-        print(i)
-        qc_mask[int(qc_ind[i])] = 1 
-        Rrs[int(qc_ind[i]),:] = Rrs_L2[i,:] 
-        exLwn[int(qc_ind[i]),:] = exLwn_L2[i,:] 
-        rho[int(qc_ind[i])] = rho_L2[i]
+    # convert L2 data to length of L1 data, with nan-padding added where no data
+    qc_mask = np.zeros(len(time_L1)) # 1 == data, 0 === no data
+    Rrs = np.nan*np.ones([len(time_L1), len(wv)])
+    exLwn = np.nan*np.ones([len(time_L1),len(wv)])
+    rho = np.nan*np.ones(len(time_L1))
+    for i in range(len(L1_matches)): # fill L1-size stuctures 
+        qc_mask[int(L1_matches[i])] = 1 
+        Rrs[int(L1_matches[i]),:] = Rrs_L2[i,:] 
+        exLwn[int(L1_matches[i]),:] = exLwn_L2[i,:] 
+        rho[int(L1_matches[i])] = rho_L2[i]
         
     return qc_mask, Rrs, exLwn, rho
 
@@ -131,16 +131,20 @@ def hyperspec_to_OLCIbands(S, time, wv, srf, srf_bands, srf_wv):
 
     # Loop for spectral down-sampling (outputs np array D)
     for j in range(len(S)): # loop over timestamps
-        wv_j = wv[np.isnan(S[j,:]) == 0] # remove nan padding (rq. for interpolation functions)
-        S_j = S[j, np.isnan(S[j,:]) == 0]    
-        for k in range(len(srf_bands)): # loop over spectral bands
-            interp_funct = interp1d(wv_j, S_j, kind = 'cubic') # interpolate to same wv interval as OLCI
-            S_j_interp = interp_funct(srf_wv[k]) # oversample on same wv range as OLCI SRF 
-            S_j_k = np.sum(S_j_interp*srf[k])/np.sum(srf[k]) # convolution of kth band for jth timestamp with OLCI SRF
-            D[j,k] =  S_j_k # 
+        if np.sum(np.isnan(S[j,:])) < len(S[j,:]): # checks spectra exists ()
+            wv_j = wv[np.isnan(S[j,:]) == 0] # remove wl bins with nan padding (rq. for interpolation functions)
+            S_j = S[j, np.isnan(S[j,:]) == 0]    
+            for k in range(len(srf_bands)): # loop over spectral bands
+                first_wv = wv[int(np.where(np.isnan(S[j,:])==0)[0][0])]  # first and last wavelengths
+                last_wv = wv[int(np.where(np.isnan(S[j,:])==0)[0][-1])]
+                if np.min(srf_wv[k]) > first_wv and np.max(srf_wv[k]) < last_wv: # tests data exists in band
+                    interp_funct = interp1d(wv_j, S_j, kind = 'cubic') # interpolate to same wv interval as OLCI
+                    S_j_interp = interp_funct(srf_wv[k]) # oversample on same wv range as OLCI SRF 
+                    S_j_k = np.sum(S_j_interp*srf[k])/np.sum(srf[k]) # convolution of kth band for jth timestamp with OLCI SRF
+                    D[j,k] =  S_j_k # 
     
     # Loop for pandas dataframe format
-    D_df = pd.DataFrame(index=time) # pandas data frame format for down-sampled spectra
+    D_df = pd.DataFrame(index = time) # pandas data frame format for down-sampled spectra
     for k in range(len(srf_bands)):
            D_df[str(srf_bands[k])] = D[:,k] 
                 
@@ -212,10 +216,10 @@ def write_spectra(D_df,sensor_info, fpath):
     return 
 
        
-def write_station_summary(summary_path, time, Es_av, Lt_av, Li_av):
+def write_station_summary(summary_path, time, Es_av, Lt_av, Li_av, Rrs_av, exLwn_av):
     ''' Function to write station summary in csv format'''
     
-    # fill up pre-amble
+    # fill up headers
     with open(summary_path, 'w', encoding='UTF8') as f:
         writer = csv.writer(f)           
         row = str('#file_type: spectrums')
@@ -239,10 +243,12 @@ def write_station_summary(summary_path, time, Es_av, Lt_av, Li_av):
         row = str('units: {"reflectance":["Wavelength [nm]","Reflectance [ / sr"],"es":["Wavelength [nm]","Irradiance [mW / (cm^2 * 10^-6 m)]"],"lt":["Wavelength [nm]","Radiance [mW / (sr * cm^2 * 10^-6 m)]"],"li":["Wavelength [nm]","Radiance [mW / (sr cm^2 * 10^-6 m)]"]}')
         writer.writerow([''.join(row)])
     
-    # append station averages to csv file - refletance and nLW to add latter
+    # append station average dataframes for each sectrum to csv file -
+    Rrs_av.to_csv(summary_path, index = False, mode = 'a')
     Es_av.to_csv(summary_path, index = False, mode = 'a')
     Lt_av.to_csv(summary_path, index = False, mode = 'a')
     Li_av.to_csv(summary_path, index = False, mode = 'a')
+    exLwn_av.to_csv(summary_path, index = False, mode = 'a')
     
     return
 
@@ -272,43 +278,50 @@ if __name__ == '__main__':
     for i in range(3): # process each station in sequence
     
         # access mat filenames (hsas data structures) of ith station 
-        # fn_L0_i = glob.glob(dir_L0 + stations[i]  + '/*mat*') 
+        # fn_L0_i = glob.glob(dir_L0 + stations[i]  + '/*mat*')  - not needed??
         fn_L1 = glob.glob(dir_L1 + stations[i]  + '/*mat*') # applies to data from ith station
         fn_L2 = glob.glob(dir_L2 + stations[i]  + '/*mat*')
         
         # unpack variables from L1 and L2 data data stuctures in np array format
-        time, windspeed, Es, Lt, Li, wv = unpack_L1(fn_L1) # applies to data from ith station 
-        # qc_mask, Rrs, exLwn, rho = unpack_L2(fn_L2, len(time) , len(wv)) # applies to data from ith station
+        time, windspeed, Es, Lt, Li, wv = unpack_L1(fn_L1) 
+        qc_mask, Rrs, exLwn, rho = unpack_L2(fn_L2, time, wv) # L2 is nan-paddeded to match length of L1
         
-        # spectral downsampling to  OLCI: element 0 is np array, 1 is dataframe formate
+        # spectral downsampling to OLCI: element 0 is np array, 1 is dataframe formate
         Es_OLCI = hyperspec_to_OLCIbands(Es, time, wv, srf, srf_bands, srf_wv)  
         Lt_OLCI = hyperspec_to_OLCIbands(Lt, time, wv, srf, srf_bands, srf_wv)
         Li_OLCI = hyperspec_to_OLCIbands(Li, time, wv, srf, srf_bands, srf_wv)
-        # multi_hyper_plot(Es, wv, Es_OLCI, srf_bands, 'Es [mW cm$^{2}$ nm$^{1}$')
+        Rrs_OLCI = hyperspec_to_OLCIbands(Rrs, time, wv, srf, srf_bands, srf_wv) 
+        exLwn_OLCI = hyperspec_to_OLCIbands(exLwn, time, wv, srf, srf_bands, srf_wv) 
+        # multi_hyper_plot(Es, wv, Es_OLCI, srf_bands, 'Es [mW cm$^{2}$ $\mu$m^{1}$')
         
         # collate sensor info and write downsampled spectra to csv files
         Es_info = {'Make': 'Seabird', 'Model': 'HypserSAS', 'Serial number': '2027A', 'Integration time': 'TO ADD', 'Raw wavelength scale': 'TO ADD'} 
         Lt_info = {'Make': 'Seabird', 'Model': 'HypserSAS', 'Serial number': '464', 'Integration time': 'TO ADD', 'Raw wavelength scale': 'TO ADD'} 
         Li_info = {'Make': 'Seabird', 'Model': 'HypserSAS', 'Serial number': '2054A', 'Integration time': 'TO ADD', 'Raw wavelength scale': 'TO ADD'} 
+        Rrs_info = {'Make': 'Seabird', 'Model': 'HypserSAS'}
+        exLwn_info = {'Make': 'Seabird', 'Model': 'HypserSAS'}
         
         Es_path = dir_write +  stations[i] + '/' + 'Es_' + stations[i] + '.csv'
         Lt_path = dir_write +  stations[i] + '/' + 'Lt_' + stations[i] + '.csv'
         Li_path = dir_write +  stations[i] + '/' + 'Li_' + stations[i] + '.csv'
+        Rrs_path = dir_write +  stations[i] + '/' + 'reflectance_' + stations[i] + '.csv'
+        exLwn_path = dir_write +  stations[i] + '/' + 'nLw_' + stations[i] + '.csv'
         
-        write_spectra(Es_OLCI[1], Es_info, Es_path)  # Es_OLCI[1] is pandas dataframe format
+        write_spectra(Es_OLCI[1], Es_info, Es_path)  # Es_OLCI[1] etc, is pandas dataframe format
         write_spectra(Lt_OLCI[1], Lt_info, Lt_path)
         write_spectra(Li_OLCI[1], Li_info, Li_path)
+        write_spectra(Rrs_OLCI[1], Rrs_info, Rrs_path)
+        write_spectra(exLwn_OLCI[1], exLwn_info, exLwn_path)
         
-        #Es_OLCI_mean, Es_OLCI_med, Es_OLCI_std = station_averages(Es_OLCI[0]) #  Es_OLCI[0] is np array format
-        #Lt_OLCI_mean, Lt_OLCI_med, Lt_OLCI_std = station_averages(Lt_OLCI[0])
-        #Li_OLCI_mean, Li_OLCI_med, Li_OLCI_std = station_averages(Li_OLCI[0])
-     
+        # perform averaging/variability for each spectra at each station -outputs dataframe that is appended to station summary in write function
         Es_av = station_averages_dataframe(Es_OLCI[0],'Es', stations[i], time, windspeed, srf_bands)
         Lt_av = station_averages_dataframe(Lt_OLCI[0],'Lt', stations[i], time, windspeed, srf_bands)
         Li_av = station_averages_dataframe(Li_OLCI[0],'Li', stations[i], time, windspeed, srf_bands)
+        Rrs_av = station_averages_dataframe(Rrs_OLCI[0],'reflectance', stations[i], time, windspeed, srf_bands) 
+        exLwn_av = station_averages_dataframe(exLwn_OLCI[0],'nLw', stations[i], time, windspeed, srf_bands)
         
         summary_path = dir_write +  stations[i] + '/' + 'Summary_' + stations[i] + '.csv'
-        write_station_summary(summary_path, time, Es_av, Lt_av, Li_av)
+        write_station_summary(summary_path, time, Es_av, Lt_av, Li_av, Rrs_av, exLwn_av) 
        
         
         
